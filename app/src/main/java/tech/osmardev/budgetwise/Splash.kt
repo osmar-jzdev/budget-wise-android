ba2203063ac86f00cb6ee74a992680608c0acfb8
osmar <techsmart.dev@gmail.com>
2023-02-01 01:25:41 -0600

    splash activity added

---
+++ app/src/main/java/tech/osmardev/budgetwise/Splash.kt
@@ -0,0 +1,21 @@
+package tech.osmardev.budgetwise
+
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.concurrent.thread
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.white)))
+        thread{
+            Thread.sleep(3000)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
