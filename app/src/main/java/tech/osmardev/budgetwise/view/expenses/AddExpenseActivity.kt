9153f8f09acec25d99adf477546b070b9d2b5183
osmar <techsmart.dev@gmail.com>
2023-02-01 23:44:33 -0600

    validations on forms income and expense

--- app/src/main/java/tech/osmardev/budgetwise/view/expenses/AddExpenseActivity.kt
+++ app/src/main/java/tech/osmardev/budgetwise/view/expenses/AddExpenseActivity.kt
@@ -1,10 +1,12 @@
 package tech.osmardev.budgetwise.view.expenses
 
+import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import tech.osmardev.budgetwise.MainActivity
 import tech.osmardev.budgetwise.R
 import tech.osmardev.budgetwise.databinding.ActivityAddExpenseBinding
 
@@ -20,10 +22,31 @@ class AddExpenseActivity : AppCompatActivity() {
         supportActionBar?.setTitle(R.string.add_expense_title)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_white)
+        onClickBtnAdd()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    private fun onClickBtnAdd() {
+        binding.btnAdd.setOnClickListener {
+            with(binding) {
+                when {
+                    txtInputExpenseName.text.toString().isEmpty() -> {
+                        txtInputExpenseName.error = resources.getString(R.string.error_empty_box)
+                    }
+                    txtInputExpenseValue.text.toString().isEmpty() -> {
+                        binding.txtInputExpenseValue.error = resources.getString(R.string.error_empty_box)
+                    }
+                    else -> {
+                        startActivity(Intent(this@AddExpenseActivity, MainActivity::class.java))
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/tech/osmardev/budgetwise/view/incomes/AddIncomeActivity.kt
+++ app/src/main/java/tech/osmardev/budgetwise/view/incomes/AddIncomeActivity.kt
@@ -1,9 +1,9 @@
 package tech.osmardev.budgetwise.view.incomes
 
-import android.graphics.drawable.ColorDrawable
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
+import tech.osmardev.budgetwise.MainActivity
 import tech.osmardev.budgetwise.R
 import tech.osmardev.budgetwise.databinding.ActivityAddIncomeBinding
 
@@ -19,10 +19,31 @@ class AddIncomeActivity : AppCompatActivity() {
         supportActionBar?.setTitle(R.string.add_income_title)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_white)
+        onClickBtnAdd()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    private fun onClickBtnAdd() {
+        binding.btnAdd.setOnClickListener {
+            with(binding) {
+                when {
+                    txtInputIncomeName.text.toString().isEmpty() -> {
+                        txtInputIncomeName.error = resources.getString(R.string.error_empty_box)
+                    }
+                    txtInputIncomeValue.text.toString().isEmpty() -> {
+                        binding.txtInputIncomeValue.error = resources.getString(R.string.error_empty_box)
+                    }
+                    else -> {
+                        startActivity(Intent(this@AddIncomeActivity, MainActivity::class.java))
+                        finish()
+                    }
+                }
+            }
+        }
     }
 }
